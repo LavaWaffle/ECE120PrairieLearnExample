7e716a854734aef66129d17eb3740a89932c660c
Hazel <bjxobjxo@gmail.com>
2025-09-01 16:24:09 -0500

    begin working on actual question :D

---
+++ questions/bases_2_10/workspace/starter_code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Grab first two inputs as integers
+    int length, width;
+    printf("Enter the length: \n");
+    scanf("%d", &length);
+    printf("Enter the width: \n");
+    scanf("%d", &width);
+    return 0;
+}
