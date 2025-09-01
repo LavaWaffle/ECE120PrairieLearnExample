73e3a7538f118eb60ea50b70c56204a1190e473c
Hazel <bjxobjxo@gmail.com>
2025-09-01 18:37:41 -0500

    fix base 2 to base 10

---
+++ questions/base_2_to_10/tests/test.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+import cgrader
+
+
+class QuestionGrader(cgrader.CGrader):
+    def tests(self):
+        self.test_compile_file(
+            "starter_code.c", "starter_code", flags="-Wall -Wextra -pedantic -Werror"
+        )
+        self.test_run(
+            "./starter_code",
+            input="101\n",
+            exp_output=[
+                "5"
+            ],
+            must_match_all_outputs="all",
+            highlight_matches=True
+        )
+        self.test_run(
+            "./starter_code",
+            input="1101\n",
+            exp_output=[
+                "13"
+            ],
+            must_match_all_outputs="all",
+            highlight_matches=True
+        )
+        self.test_run(
+            "./starter_code",
+            input="11111\n",
+            exp_output=[
+                "31"
+            ],
+            must_match_all_outputs="all",
+            highlight_matches=True
+        )
+
+
+g = QuestionGrader()
+g.start()
---
+++ questions/base_2_to_10/workspace/starter_code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Get N
+    int N;
+    scanf("%d", &N); // N is unsigned between 0 and 111111 (base 2)
+
+    // Your code here (convert from base 2 to base 10)
+    // Dont forget to print the result!
+
+
+    // Keep this return 0;
+    return 0;
+}
