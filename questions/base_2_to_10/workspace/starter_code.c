b1622338305bf242bde84ce503885ef7ce8fcd05
Hazel <bjxobjxo@gmail.com>
2025-09-03 15:58:54 -0500

    clarify 2 to 10

--- questions/base_2_to_10/workspace/starter_code.c
+++ questions/base_2_to_10/workspace/starter_code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
+// Convert from base 2 to base 10
 int main() {
     int result = 0;
-    int power = 1 << 5;
-
+    int power = 1 << 5; // 32 (MSB value of a 6 bit unsigned binary)
 
     while (1) {
         char nextBit;
